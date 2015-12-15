a59a25897c0869953389645aea10bd848f35eb72
Xavier Perseguers <xavier@causal.ch>
2015-12-15 08:32:14 +0100

    [TASK] Check PHP7 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.2.0',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.7-5.6.99',
+            'php' => '5.3.7-7.0.99',
             'typo3' => '6.2.0-7.99.99',
         ),
         'conflicts' => array(),
