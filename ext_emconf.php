9825de039db0d204c5a550213366ac07f408b9dd
Xavier Perseguers <xavier@causal.ch>
2017-04-25 09:17:32 +0200

    [TASK] Adapt dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.3.7-7.1.99',
-            'typo3' => '6.2.0-8.99.99',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
